7472a3655991dbd987274a608519e1780b99c99d
Josh Varty <joshvarty@gmail.com>
2018-02-25 21:30:47 -0500

    Add 2D Countour Plot

--- OptimizerAnimations/2DContourPlotWithGradientField.py
+++ OptimizerAnimations/2DContourPlotWithGradientField.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import autograd.numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.colors import LogNorm
+from autograd import elementwise_grad
 
 f  = lambda x, y: (1.5 - x + x*y)**2 + (2.25 - x + x*y**2)**2 + (2.625 - x + x*y**3)**2
 xmin, xmax, xstep = -4.5, 4.5, .2
@@ -14,16 +15,17 @@ z = f(x, y)
 minima = np.array([3., .5])
 minima_ = minima.reshape(-1, 1)
 
-fig = plt.figure(figsize=(8, 5))
-ax = plt.axes(projection='3d', elev=50, azim=-50)
+dz_dx = elementwise_grad(f, argnum=0)(x, y)
+dz_dy = elementwise_grad(f, argnum=1)(x, y)
 
-ax.plot_surface(x, y, z, norm=LogNorm(), rstride=1, cstride=1, 
-                edgecolor='none', alpha=.8, cmap=plt.cm.jet)
-ax.plot(*minima_, f(*minima_), 'r*', markersize=10)
+fig, ax = plt.subplots(figsize=(10, 6))
+
+ax.contour(x, y, z, levels=np.logspace(0, 5, 35), norm=LogNorm(), cmap=plt.cm.jet)
+ax.quiver(x, y, x - dz_dx, y - dz_dy, alpha=.5)
+ax.plot(*minima_, 'r*', markersize=18)
 
 ax.set_xlabel('$x$')
 ax.set_ylabel('$y$')
-ax.set_zlabel('$z$')
 
 ax.set_xlim((xmin, xmax))
 ax.set_ylim((ymin, ymax))
